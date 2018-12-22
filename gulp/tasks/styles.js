222f7f06463306a752f02ddbbed7c7292b4aad5d
Brian <briansenpai14@gmail.com>
2018-12-22 12:22:44 +0200

    completed the testimonials section

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,15 +1,15 @@
-let gulp = require('gulp'), 
+var gulp = require('gulp'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nestedcss = require('postcss-nested'),
-cssimport = require('postcss-import'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
 mixins = require('postcss-mixins');
 
-gulp.task('styles', function () {
+gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssimport, mixins, cssvars, nestedcss, autoprefixer]))
-    .on('error', function (errorInfo) {
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on('error', function(errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
     })
